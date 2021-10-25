349c52af4be6c8b9f7013df2eb4c075b2a968abc
lawrenceagles <lawrenceagles@gmail.com>
2021-10-25 11:53:02 +0100

    added render room

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import {
 	selectIsLocalAudioEnabled
 } from '@100mslive/hms-video-store';
 
+import { getToken, createElem } from './utils';
+
 const hms = new HMSReactiveStore();
 const hmsStore = hms.getStore();
 const hmsActions = hms.getHMSActions();
@@ -48,9 +50,80 @@ Form.addEventListener('submit', async function handleSubmit(e) {
 });
 
 // handle join room view
+function handleConnection(isConnected) {
+	if (isConnected) {
+		console.log('connected');
+		// hides Form
+		FormView.classList.toggle('hidden');
+		// displays room
+		RoomView.classList.toggle('hidden');
+	} else {
+		console.log('disconnected');
+		// hides Form
+		FormView.classList.toggle('hidden');
+		// displays room
+		RoomView.classList.toggle('hidden');
+	}
+}
+
+// subscribe to room state
+hmsStore.subscribe(handleConnection, selectIsConnectedToRoom);
 
 // leave room
 
+function leaveRoom() {
+	hmsActions.leave();
+	JoinBtn.innerHTML = 'Join';
+}
+LeaveRoomBtn.addEventListener('click', leaveRoom);
+window.onunload = leaveRoom;
+
 // display room
 
+function renderPeers(peers) {
+	PeersContainer.innerHTML = ''; // clears the container
+	if (!peers) {
+		// this allows us to make peer list an optional argument
+		peers = hmsStore.getState(selectPeers);
+	}
+	peers.forEach((peer) => {
+		// creates an image tag
+		const peerAvatar = createElem('img', {
+			class: 'object-center object-cover w-full h-full',
+			src: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+			alt: 'photo'
+		});
+
+		// create a description paragrah tag with a text
+		peerDesc = createElem(
+			'p',
+			{
+				class: 'text-white font-bold'
+			},
+			peer.name + '-' + peer.roleName
+		);
+		const peerContainer = createElem(
+			'div',
+			{
+				class:
+					'w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center'
+			},
+			peerAvatar,
+			peerDesc
+		);
+
+		// appends children
+		PeersContainer.append(peerContainer);
+	});
+}
+hmsStore.subscribe(renderPeers, selectPeers);
+
 //handle mute/unmute peer
+
+AudioBtn.addEventListener('click', () => {
+	let audioEnabled = hmsStore.getState(selectIsLocalAudioEnabled);
+	AudioBtn.innerText = audioEnabled ? 'Mute' : 'Unmute';
+	AudioBtn.classList.toggle('bg-green-600');
+	AudioBtn.classList.toggle('bg-red-600');
+	hmsActions.setLocalAudioEnabled(!audioEnabled);
+});
--- utils/createElem.js
+++ utils/createElem.js
@@ -1,2 +1,13 @@
-const createElem = () => {};
+// helper function to create html elements
+function createElem(tag, attrs = {}, ...children) {
+	const newElement = document.createElement(tag);
+	Object.keys(attrs).forEach((key) => {
+		newElement.setAttribute(key, attrs[key]);
+	});
+
+	children.forEach((child) => {
+		newElement.append(child);
+	});
+	return newElement;
+}
 export default createElem;
